639708722ffc650c1656192df6a19bf70b2f25d4
Konstantin Pogodin <auronus@mail.ru>
2024-06-26 19:22:03 +0300

    XML - JSON парсер

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,17 @@ import com.opencsv.CSVReader;
 import com.opencsv.bean.ColumnPositionMappingStrategy;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
+import org.apache.commons.logging.Log;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
+import javax.management.Attribute;
+import javax.sql.rowset.spi.XmlReader;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -19,8 +29,11 @@ public class Main {
         String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
         String fileName = "data.csv";
         List<Employee> list = parseCSV(columnMapping, fileName);
-        String json = listToJson(list);
-        writeString(json);
+        writeString(listToJson(list), "data.json");
+
+        List<Employee> xmlList = parseXML("data.xml");
+        writeString(listToJson(xmlList), "data2.json");
+
     }
 
     private static List<Employee> parseCSV(String[] columnMapping, String fileName) {
@@ -42,7 +55,7 @@ public class Main {
     }
 
     private static String listToJson(List<Employee> list) {
-        String json = "";
+        String json;
         Type listType = new TypeToken<List<Employee>>() {
         }.getType();
         GsonBuilder builder = new GsonBuilder();
@@ -51,11 +64,39 @@ public class Main {
         return json;
     }
 
-    private static void writeString(String json) {
-        try (FileWriter fileWriter = new FileWriter("data.json")) {
+    private static void writeString(String json, String fileName) {
+        try (FileWriter fileWriter = new FileWriter(fileName)) {
             fileWriter.write(json);
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private static List<Employee> parseXML(String fileName) {
+        List<Employee> list = new ArrayList<>();
+        try {
+            DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+            Document doc = documentBuilder.parse(new File(fileName));
+            Node root = doc.getDocumentElement();
+            NodeList nodeList = root.getChildNodes();
+
+            for (int i = 0; i < nodeList.getLength(); i++) {
+                Node node = nodeList.item(i);
+                if (Node.ELEMENT_NODE == node.getNodeType()) {
+                    Element employee = (Element) node;
+                    list.add(new Employee(
+                            Long.parseLong(employee.getElementsByTagName("id").item(0).getTextContent()),
+                            employee.getElementsByTagName("firstName").item(0).getTextContent(),
+                            employee.getElementsByTagName("lastName").item(0).getTextContent(),
+                            employee.getElementsByTagName("country").item(0).getTextContent(),
+                            Integer.parseInt(employee.getElementsByTagName("age").item(0).getTextContent()))
+                    );
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return list;
     }
 }
